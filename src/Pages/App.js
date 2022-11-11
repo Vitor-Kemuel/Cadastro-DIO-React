61009a80a53a47ad330a614dd0806e567c8b2094
Vitor-Kemuel <kemuvitor@gmail.com>
2022-11-11 00:10:50 -0300

    implementando aplicação

---
+++ src/Pages/App.js
@@ -0,0 +1,12 @@
+import { Register } from "./register";
+import { Container } from "./styles";
+
+function App() {
+  return (
+    <Container>
+      <Register />
+    </Container>
+  );
+}
+
+export default App;
